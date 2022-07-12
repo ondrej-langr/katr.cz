c885a6af6d96c686b887a7e9df3e56dbeb150995
Ondřej Langr <hi@ondrejlangr.cz>
2022-07-12 23:54:02 +0200

    Get data in controller routes

--- modules/Katr/front.routes.php
+++ modules/Katr/front.routes.php
@@ -191,14 +191,18 @@ $router->get('/', function (
       ],
       'title' => getSetting('main_page_title')['content']['data'],
       'description' => getSetting('main_page_description')['content']['data'],
-      'about' => getSetting('main_page_about')['content']['data'],
+      'about' => [
+        'content' => getSetting('main_page_about')['content']['data'],
+      ],
       'blog_list' => [
         'before' => getSetting('main_page_blog_list_before')['content']['data'],
         'after' => getSetting('main_page_blog_list_after')['content']['data'],
+        'image' => getSetting('main_page_blog_list_image')['content']['data'],
       ],
       'positions' => [
         'before' => getSetting('main_page_positions_before')['content']['data'],
         'after' => getSetting('main_page_positions_after')['content']['data'],
+        'image' => getSetting('main_page_positions_image')['content']['data'],
       ],
     ])
   );
