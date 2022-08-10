5b1dd2055f70dce6e801ed2b261a0bcb545a1f51
Ondřej Langr <hi@ondrejlangr.cz>
2022-08-10 22:17:49 +0200

    Get latest fixes and new admin from PromCMS

---
+++ public/admin/assets/index.943b0941.js
@@ -0,0 +1,1 @@
+import{U as r}from"./index.2969125f.js";import{j as e,Z as t}from"./index.db4443a5.js";const n=()=>e(t,{children:e(r.ContextProvider,{view:"update",children:e(r.Content,{})})});export{n as default};
---
+++ public/admin/assets/invite.777e3401.js
@@ -0,0 +1,1 @@
+import{U as r}from"./index.2969125f.js";import{j as e,Z as t}from"./index.db4443a5.js";const s=()=>e(t,{children:e(r.ContextProvider,{view:"create",children:e(r.Content,{})})});export{s as default};
