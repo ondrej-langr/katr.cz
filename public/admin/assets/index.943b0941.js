4f80d47cf064527b5629937e6eda995f7a4d3a4c
Ondřej Langr <hi@ondrejlangr.cz>
2022-08-11 01:38:29 +0200

    Get latest prom with localizations

--- public/admin/assets/index.943b0941.js
+++
@@ -1,1 +0,0 @@
-import{U as r}from"./index.2969125f.js";import{j as e,Z as t}from"./index.db4443a5.js";const n=()=>e(t,{children:e(r.ContextProvider,{view:"update",children:e(r.Content,{})})});export{n as default};
---
+++ public/admin/assets/invite.b9a6ef14.js
@@ -0,0 +1,1 @@
+import{U as r}from"./index.dff39219.js";import{j as e,C as t}from"./index.edffba3d.js";const s=()=>e(t,{children:e(r.ContextProvider,{view:"create",children:e(r.Content,{})})});export{s as default};
