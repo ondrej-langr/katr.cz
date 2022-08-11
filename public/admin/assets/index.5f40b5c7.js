3a9a5913e9655aea9539c3d736429f8ffc4c9c5e
Ondřej Langr <hi@ondrejlangr.cz>
2022-08-11 16:14:52 +0200

    Update admin

--- public/admin/assets/index.5f40b5c7.js
+++
@@ -1,1 +0,0 @@
-import{U as r}from"./index.dff39219.js";import{j as e,C as t}from"./index.edffba3d.js";const n=()=>e(t,{children:e(r.ContextProvider,{view:"update",children:e(r.Content,{})})});export{n as default};
---
+++ public/admin/assets/index.b85e110e.js
@@ -0,0 +1,1 @@
+import{U as r}from"./index.7ac6d4bd.js";import{j as e,C as t}from"./index.e5661ece.js";const n=()=>e(t,{children:e(r.ContextProvider,{view:"update",children:e(r.Content,{})})});export{n as default};
