9a192c1f1c57c60e978272fadbff5b1d6edc62b7
Ondřej Langr <hi@ondrejlangr.cz>
2022-09-20 06:52:14 +0200

    Make big-gallery block be able to link position from url when clicking on input

--- frontend-src/index.ts
+++ frontend-src/index.ts
@@ -20,4 +20,32 @@ import './lib/alpine'
   }).listen('.zooming')
 
   lightbox.init()
+
+  const onTabClick = (e: Event) => {
+    window.location.hash = (e.currentTarget as HTMLInputElement).getAttribute(
+      'id'
+    )!
+  }
+
+  document
+    .querySelectorAll<HTMLInputElement>('.tabs > input')
+    .forEach((elMatch) => {
+      elMatch.addEventListener('change', onTabClick)
+    })
+
+  if (window.location.hash.startsWith('#tabs-tab--')) {
+    document.querySelectorAll<HTMLDivElement>('.tabs').forEach((elMatch) => {
+      elMatch
+        .querySelectorAll<HTMLInputElement>(':scope > input')
+        .forEach((inputEl) => {
+          inputEl.checked = false
+
+          if (
+            inputEl.getAttribute('id') === window.location.hash.replace('#', '')
+          ) {
+            inputEl.checked = true
+          }
+        })
+    })
+  }
 })()
