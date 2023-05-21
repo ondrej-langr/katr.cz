a3eed3fe85a2b95f32bcf638fc3077538dad10d4
Ondřej Langr <hi@ondrejlangr.cz>
2023-05-21 23:40:25 +0200

    Update packages and rebuild admin

--- public/admin/assets/page.6425f83d.js
+++
@@ -1,1 +0,0 @@
-import{j as e,C as r}from"./index.43307782.js";import{C as t,a}from"./Content.4f916565.js";const s=()=>e(r,{children:e(t,{view:"update",children:e(a,{})})});export{s as default};
---
+++ public/admin/assets/page.965280ab.js
@@ -0,0 +1,1 @@
+import{j as e,C as r}from"./index.bba12bdf.js";import{C as t,a}from"./Content.5a530061.js";const n=()=>e(r,{children:e(t,{view:"create",children:e(a,{})})});export{n as default};
