f77044240f2fb3bca540bbe7b6e7baf7a445b160
Ondřej Langr <hi@ondrejlangr.cz>
2023-09-18 09:35:21 +0200

    Update prom to latest and get newest admin

---
+++ public/admin/assets/page.3d84f958.js
@@ -0,0 +1,1 @@
+import{j as e,C as r}from"./index.b593dde9.js";import{C as t,a}from"./Content.d70a05a9.js";const s=()=>e(r,{children:e(t,{view:"update",children:e(a,{})})});export{s as default};
---
+++ public/admin/assets/page.4cedab7b.js
@@ -0,0 +1,1 @@
+import{j as e,C as r}from"./index.b593dde9.js";import{C as t,a}from"./Content.d70a05a9.js";const n=()=>e(r,{children:e(t,{view:"create",children:e(a,{})})});export{n as default};
